84676b2af7fb58a867c901468edd80bdd500e264
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-26 23:24:39 +0200

    Create Main View

--- iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/AppDelegate.swift
@@ -13,12 +13,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		
+		// Setup
+		setupView()
+		
 		return true
 	}
 
 	
-
+	// MARK: - Private Methods
+	
+	// En setupView el indicamos nuestra primera vista
+	private func setupView() {
+		
+		// Instanciamos window
+		window = UIWindow(frame: UIScreen.main.bounds)
+		// Instanciamos nuestro View Controller
+		let vc = HomeViewController()
+		// Le decimos que nuestra primera vista será nuestro VC
+		window?.rootViewController = vc
+		window?.makeKeyAndVisible()
+		
+		
+	}
 	
 
 
---
+++ iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Rick Larios on 26/6/22.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+	
+	// MARK: Initialization
+	
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	// MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+	       
+    }
+
+
+
+}
